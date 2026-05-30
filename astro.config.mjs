652761f67ac06770cfabd4c042172b218db979e0
Sam Han <sshan95@SeriousS3M.lan>
2026-05-29 21:12:10 -0400

    Add Github pages deploy workflow for new site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 
 export default defineConfig({
+  site: "https://medicoder.ai",
   integrations: [svelte()],
 });
